773bb4837ef4608e4a73dd8ada714ff341076eb1
Josh Duff <me@JoshDuff.com>
2017-05-27 18:08:15 -0500

    Split the two tables apart, represent "shown" in a separate data structure

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
 import Channels from './Channels.html'
 
-const channelComponent = new Channels({
+new Channels({
 	target: document.querySelector('main'),
 	data: {
 		channelPickMode: false,
 	}
 })
-
-channelComponent.on('shown-clicked', event => {
-	if (channelComponent.get('channelPickMode')) {
-		const channels = channelComponent.get('channels')
-
-		const filteredChannels = channels.map(function(channel, index) {
-			if (index === event.i) {
-				return Object.assign(channel, { shown: !channel.shown })
-			} else {
-				return channel
-			}
-		})
-
-		channelComponent.set({ channels: filteredChannels })
-	}
-})
-
